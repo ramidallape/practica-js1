b16d9560478d21b951aaf155fa5d7d19157fec6e
ramidallape <ramidallape12@gmail.com>
2023-07-11 21:12:55 -0300

    practicando en consola

---
+++ main.js
@@ -0,0 +1,118 @@
+// 1°
+
+// function ParImpar(number) {
+//     if (number % 2 === 0) {
+//         console.log ("El numero es par")
+//     }
+//     else {
+//         console.log ("El numero es impar")
+//     }
+// }
+
+// ParImpar (8);
+
+//2°
+
+// const num1 = 12;
+// const num2 = 15;
+
+// function MayorMenor(num1, num2) {
+//     if (num1 > num2) {
+//         console.log ("El número " + num1 + " es mayor a " + num2);
+//     }
+    
+//     if (num1 < num2) {
+//         console.log ("El número " + num2 + " es mayor a " + num1);
+//     }
+
+//     if (num1 == num2) {
+//         console.log ("Los números son iguales");
+//     }
+// }
+
+// MayorMenor (num1, num2);
+
+
+//3°
+
+// const numero = 25;
+// function Multiplode5 (numero) {
+//     if (numero > 0 && numero % 5 == 0) {
+//         console.log ("El número " + numero + " es múltiplo de 5")
+//     }
+//     else {
+//         console.log ("El número " + numero + " no es múltiplo de 5")
+//     }    
+// }
+
+// Multiplode5 (numero);
+
+
+//4°
+
+// desde = 0;
+// param = 15;
+
+// function contar(desde, param) {
+//     while (desde <= param) {
+//         console.log (desde);
+//         desde++;
+//     }
+// }
+
+// contar (desde, param);
+
+//5
+
+
+// let cadena = " Valkirie ";
+// let xveces = 6;
+
+// function CadenaRepetida(cadena, xveces) {
+//   let cadenaRepetida = cadena.repeat(xveces);
+//   console.log(cadenaRepetida)
+// }
+
+// CadenaRepetida (cadena, xveces);
+
+
+//6°
+
+// const amigos = ["Paula", "Gonzalo", "Yisus", "Nacho"];
+
+// function array (amigos) {
+//     console.log(amigos)
+// }
+// array(amigos);
+
+
+
+//7°
+
+// const contador = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// function MenosEL5(contador) {
+//     for (let i = 0; i < contador.length; i++) {
+//       if (i !== 4) {
+//         console.log(contador[i]);
+//       }
+//     }
+//   } 
+//   MenosEL5 (contador);
+
+
+
+  //8°
+
+
+
+// function Multiplicacion(arrays, numm) {
+//   for (let i = 0; i < arrays.length; i++) {
+//     console.log(arrays[i] * numm);
+//   }
+// }
+
+// const arrays = [4, 8, 12, 15, 20];
+// const numm = 3;
+// Multiplicacion(arrays, numm);
+
